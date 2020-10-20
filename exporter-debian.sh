003aaaade8c9bce1e92060c79df2fdd5beb8f7a6
CollinLeishman <40609474+CollinLeishman@users.noreply.github.com>
2020-10-20 15:31:20 -0600

    Create exporter-debian.sh
    initial commit

---
+++ exporter-debian.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -xe
+
+
+#Create User
+adduser --no-create-home --disabled-login --shell /bin/false --gecos "Node Exporter User" node_exporter
+#Download Binary
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+#Untar
+tar xvzf node_exporter-1.0.1.linux-amd64.tar.gz
+#Copy Expoter
+cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
+#Assign Ownership Again
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+#Creating Service File
+cat <<- EOF >  /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+
+export LC_ALL=en_US.UTF-8;
+export LANG=en_US.UTF-8;
+systemctl daemon-reload;
+systemctl enable node_exporter
+systemctl start node_exporter
+systemctl status node_exporter
